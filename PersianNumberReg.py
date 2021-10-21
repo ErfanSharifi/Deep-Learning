015bd657f284602eeff6e716915f3b0b171383f1
Erfan Sharifi <e.sharify@gmail.com>
2021-10-21 12:25:21 +0330

    Hello

--- PersianNumberReg.py
+++ PersianNumberReg.py
@@ -1,6 +1,6 @@
 from tensorflow import keras
 import keras
-from keras.models import sequential
+from keras.models import Sequential
 from keras.layers import Dense, Activation
 import numpy as np
 from dataset import load_hoda
@@ -30,6 +30,7 @@ def print_data_info(x_train, y_train, x_test, y_test):
 
     print ("\ty_train[0]:{}".format(y_train[0]))
 
+# Preprocessing
 
 x_train = np.array(x_train_original)
 y_train = np_utils.to_categorical(y_train_original, num_classes=10)
@@ -43,3 +44,34 @@ print_data_info(x_train_original, y_train_original, x_test_original, y_test_orig
 
 print ("After Preprocessing")
 print_data_info(x_train, y_train, x_test, y_test)
+
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+model = Sequential()
+model.add(Dense(64, activation = 'relu', input_dim = 25))
+model.add(Dense(10, activation = 'softmax'))
+
+model.summary()
+
+# Compile Model
+
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          epochs=30,
+          batch_size=64, validation_split=0.2)
+
+# model.compile(loss='categorical_crossentropy',optimizer = 'rmsprop',metrics = ['accuracy'])
+
+# model.fit(x_train, y_train, epochs=30,batch_size = 64,validation_split=0.2)
+
+# loss, acc = model.evaluate(x_test, y_test)
+# print('\nTesting loss: %.2f, acc: %.2f%%'%(loss, acc))
+
